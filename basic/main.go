97a9e060150aea60631e5faaded3be8b4d1a2302
Jonathan Lo <jonlo1987@gmail.com>
2021-04-14 12:59:11 -0400

    Added Golang basics

---
+++ basic/main.go
@@ -0,0 +1,31 @@
+package main
+
+// Imports are done this way.
+import "fmt"
+
+func main() {
+
+	// Variables can be declared with var.
+	var b string
+
+	// Variables can also be declared in a shorter way :=
+	// No type needed.
+	// The colons means we're declaring the variable.
+	// So we can't use := on the same variable twice.
+	a := "hello"
+
+	// Once a variable is declared, we use regular =
+	a = "world"
+
+	// If does not need parenthesis around conditions like in Java.
+	if a == "world" {
+		fmt.Println("a is hello")
+	} else {
+		fmt.Println("a is not hello")
+	}
+
+	// Type checking with ==
+	if b == "" {
+		fmt.Println("Primitve zero values are always not nil")
+	}
+}
