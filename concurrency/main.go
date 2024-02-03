d7ac0086b7e2d81f96e1cd8f189fcc822839e8ee
Jonathan Lo <jonlo1987@gmail.com>
2024-02-03 11:15:45 -0500

    Added new concurrency code - go routines

---
+++ concurrency/main.go
@@ -0,0 +1,38 @@
+/*
+Concurrency allows us to have multiple blocks of code share the
+execution time by pausing their execution normally. We can also
+run blocks of code in parallel at the same time. Concurrent tasks
+in Go are called go routines, and not threads. To execute multiple
+functions in new go routines, all we need to do is add the word go
+in front of the function.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pl = fmt.Println
+
+func printTo15() {
+	for i := 1; i <= 15; i++ {
+		pl("Fun 1 :", i)
+	}
+}
+
+func printTo10() {
+	for i := 1; i <= 10; i++ {
+		pl("Fun 1 :", i)
+	}
+}
+
+func main() {
+	go printTo15()
+	go printTo10()
+
+	// We need to pause the execution of the main function
+	// for other functions to execute
+	time.Sleep(2 * time.Second)
+}
